7961b1dc313a2343b4f797558f12eb0f6feaae85
tonbei10 <47322051+tonbei10@users.noreply.github.com>
2021-10-31 22:46:21 +0900

    サーバータイプ判別処理の追加(Spigot or PaperMC)
    サーバータイプ判別処理の追加(Spigot or PaperMC)
    無効な文字列の修正

--- src/main/java/com/tonbei/archangelsbow/ArchangelsBow.java
+++ src/main/java/com/tonbei/archangelsbow/ArchangelsBow.java
@@ -1,14 +1,36 @@
 package com.tonbei.archangelsbow;
 
+import org.bukkit.entity.Entity;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.lang.reflect.Method;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public final class ArchangelsBow extends JavaPlugin implements Listener {
+
+    private boolean isDebug = true;
+
+    static Logger logger = null;
+
+    private boolean isPaperMC = false;
+    private Method isTicking = null;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-        getServer().getPluginManager().registerEvents(this, this);
+        this.getServer().getPluginManager().registerEvents(this, this);
+        if(logger == null) logger = this.getLogger();
+
+        try {
+            isTicking = Entity.class.getMethod("isTicking");
+            isPaperMC = true;
+        } catch (NoSuchMethodException e) {
+            isPaperMC = false;
+        }
+
+        if(isDebug) logger.log(Level.INFO, "Server Type : " + (isPaperMC ? "PaperMC" : "Not PaperMC"));
     }
 
     @Override
